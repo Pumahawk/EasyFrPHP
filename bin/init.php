0845bbfc8630d7755c0d6d570797c1228096b2ae
Lorenzo Gandino <lorenzoga94@gmail.com>
2018-03-19 13:32:14 +0100

    Aggiunto supporto composer, aggiunte variabili globali

--- bin/init.php
+++ bin/init.php
@@ -3,3 +3,9 @@
 set_include_path(__DIR__.'/../');
 require('bin/autoload.php');
 @include('app/init.php');
+require('vendor/autoload.php');
+
+// Define different global variable.
+
+// Define resources path
+define("FR_PATH_RESOURCES", __DIR__."/../resources");
